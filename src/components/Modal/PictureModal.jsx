99dd1dd78744cd0209d96248cb5eec3e14231fb3
ezrabales <bales.ezra@gmail.com>
2026-05-24 17:57:31 -0600

    add picture modal

--- src/components/Modal/PictureModal.jsx
+++ src/components/Modal/PictureModal.jsx
@@ -6,30 +6,44 @@ import { tempPhoto } from "../../assets";
 const PictureModal = () => {
   const { modalOpen, setModalOpen } = useGlobal();
   const photos = [
-    tempPhoto,
-    tempPhoto,
-    tempPhoto,
-    tempPhoto,
-    tempPhoto,
-    tempPhoto,
-    tempPhoto,
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
+    { src: tempPhoto, description: "description" },
   ];
 
   if (modalOpen !== "pictures") return;
   return (
     <Modal title={"Pictures"}>
-      <div
-        className="pic"
-        onWheel={(e) => {
-          e.currentTarget.scrollLeft += e.deltaY * 1.2;
-        }}
-      >
-        <div className="pic__track">
-          {photos.map((photo, i) => (
-            <img key={i} src={photo} alt="" className="pic__pic" />
-          ))}
+      {photos.length <= 0 ? (
+        <>
+          <div className="pic__none">No pictures</div>
+          <button className="pic__add-pic-btn">Add Pictures</button>
+        </>
+      ) : (
+        <div
+          className="pic"
+          onWheel={(e) => {
+            e.currentTarget.scrollLeft += e.deltaY * 1.2;
+          }}
+        >
+          <div className="pic__track">
+            {photos.map((photo, i) => {
+              return (
+                <div key={i}>
+                  <img src={photo.src} alt="" className="pic__pic" />
+                  <p className="pic__description">{photo.description}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
+      <button className="pic__add-pic-btn">Add Pictures</button>
     </Modal>
   );
 };
